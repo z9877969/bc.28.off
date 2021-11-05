2e2230d1930a03b13ddae9d80709b79aa07dbcd7
z9877969 <z9877969@ukr.net>
2021-11-05 11:16:37 +0200

    adds app refactoring

--- lesson-9(rest-api)/code/src/App.jsx
+++ lesson-9(rest-api)/code/src/App.jsx
@@ -1,103 +1,27 @@
 import { Component } from "react";
-import { getArticles } from "./service/api";
+import Form from "./components/Form/Form";
+import NewsList from "./components/NewsList/NewsList";
 import "./App.css";
-import s from "./news.module.css";
-import defaultImg from "./assets/images/default-image.png";
 // console.log(process.env.REACT_APP_API_KEY);
 
 class App extends Component {
   state = {
-    articles: [], // => []
-    error: null,
-    isLoading: false,
-    query: "all", // => rocket
-    inputQuery: "",
-    page: 1, // 5 => 1
+    query: "all",
   };
 
-  // componentDidMount() {
-  //   this.setState({ isLoading: true });
-  //   getArticles(this.state.query)
-  //     .then((articles) => this.setState({ articles: articles }))
-  //     .catch((err) => this.setState({ error: err }))
-  //     .finally(() => this.setState({ isLoading: false }));
-  // }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      this.state.query !== prevState.query ||
-      this.state.page !== prevState.page
-    ) {
-      this.getNews();
-    }
-    // if (this.state.page !== prevState.page && this.state.page !== 1) {
-    //   this.getNews();
-    // }
-  }
-
-  getNews = () => {
-    this.setState({ isLoading: true });
-    getArticles(this.state.query, this.state.page)
-      .then((articles) =>
-        this.setState((prev) => ({
-          articles: [...prev.articles, ...articles],
-        }))
-      )
-      .catch((err) => this.setState({ error: err }))
-      .finally(() => this.setState({ isLoading: false }));
-  };
-
-  handleChange = (e) => {
-    this.setState({ inputQuery: e.target.value });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({ query: this.state.inputQuery, page: 1, articles: [] });
-  };
-
-  handleChangePage = () => {
-    this.setState((prevState) => ({ page: prevState.page + 1 }));
+  changeQuery = (query) => {
+    this.setState({ query });
   };
 
   render() {
+    const { query } = this.state;
+    // if(err) return <h1>{err}</h1>
+
     return (
       <div className="App">
         {this.state.isLoading && <p>Loading...</p>}
-        <form className="form" onSubmit={this.handleSubmit}>
-          <input
-            className="input"
-            type="text"
-            placeholder="Search..."
-            value={this.state.inputQuery}
-            onChange={this.handleChange}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        {this.state.articles.length > 0 && (
-          <ul className={s.news}>
-            {this.state.articles.map((item) => (
-              <li className={s.item}>
-                <img
-                  className={s.img}
-                  src={item.urlToImage || defaultImg}
-                  alt=""
-                />
-                <div className={s.textWrapper}>
-                  <h3 classNames={s.title}>{item.title}</h3>
-                  <p className={s.author}>{item.author}</p>
-                  <p className={s.date}>{item.publishedAt}</p>
-                  <p className={s.descr}>{item.description}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-        {this.state.articles.length%12 === 0 && (
-          <button className="btn" type="button" onClick={this.handleChangePage}>
-            More
-          </button>
-        )}
+        <Form cbOnSubmit={this.changeQuery} />
+        <NewsList query={query} />
       </div>
     );
   }
